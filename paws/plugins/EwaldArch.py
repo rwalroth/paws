8e76232692acf1c37854fd59e96579209452ea5d
Walroth <walroth@slac.stanford.edu>
2019-09-06 14:37:49 -0700

    implemented integrate1d in EwaldArch
    EwaldArch now has a functional integrate1d method. Still
    needs thorough testing and integrate2d to be implemented
    for first real iteration.

--- paws/plugins/EwaldArch.py
+++ paws/plugins/EwaldArch.py
@@ -45,6 +45,10 @@ data contained in an EwaldArch object.
 import sys
 import os
 from threading import Condition
+from pyFAI.azimuthalIntegrator import AzimuthalIntegrator
+from pyFAI import units
+import numpy as np
+
 from .PawsPlugin import PawsPlugin
 
 from .. import pawstools
@@ -56,17 +60,15 @@ class EwaldArch(PawsPlugin):
 
     Attributes:
         map_raw: numpy 2d array of the unprocessed image data
-        map_corr: image data corrected for any masks, flat filed,
-            dark current, etc corrections
-        map_xyz: rotated Cartesian coordinates for the pixels
-        map_tpr: rotated spherical coordinates for the pixels (theta, phi, r)
-        map_q: reciprocal space coordinates for the 2D data
+        mask: map of pixels to be masked out of integration
+        PONI: poni data for integration
         int_raw: raw integrated 1D pattern
         int_pcount: values to use for normalizing the 1D pattern by number
             of pixels in each two-theta bin
         int_norm: normalized 1D pattern
         int_2theta: 1D 2 theta array
         int_q: q values for 1D array
+        map_q: reciprocal space coordinates for data
         scan_info: information from any relevant motors and sensors
         rotations: dictionary of the order of rotations to be done,
             and the angles to apply
@@ -74,20 +76,87 @@ class EwaldArch(PawsPlugin):
             access data at a time
 
     Methods:
-        read_file: read in data from file as a numpy array
-        apply_corrections: apply any corrections needed for the data
-        rotate: rotates the provided xyz_map based on the provided
-            rotation order and angles
-        cart_to_sphere: converts Cartesian coordinates to spherical
-        sphere_to_q: converts spherical coordinates to reciprocal space
         integrate_1d: integrate the image data to create I, 2theta, q, and
             normalization arrays
-        read_raw (static): reads in raw file format and returns a numpy array
-        mask: applies a provided mask to make any undesired pixels negative
-        rot_x (static): rotates about x axis (orthogonal to beam, horizontal)
-        rot_y (static): rotates about the y axis (orthogonal to beam, vertical)
-        rot_z (static): rotates about the z axis (collinear with the beam)
+        integrate_2d: integrate the image data in 2D
     """
     
-    def __init__(self, file=None, map_raw=None):
-        pass
+    def __init__(self, map_raw=None, PONI=None, mask=None, scan_info=None):
+        self.map_raw = map_raw
+        self.mask = mask
+        self.PONI = PONI
+        self.scan_info = scan_info
+        self.arch_lock = Condition()
+        self.map_norm = None
+        self.map_q = None
+        self.int_1d_raw = None
+        self.int_1d_pcount = None
+        self.int_1d_norm = None
+        self.int_1d_2theta = None
+        self.int_1d_q = None
+        self.int_2d_raw = None
+        self.int_2d_pcount = None
+        self.int_2d_norm = None
+        self.int_2d_2theta = None
+        self.int_2d_q = None
+    
+    
+    def integrate_1d(self, numpoints=10000, radial_range=[0,180],
+                     monitor='i0',  unit=units.TTH_DEG, **kwargs):
+        with self.arch_lock:
+            self.map_norm = self.map_raw/self.scan_info[monitor]
+            
+            ai = AzimuthalIntegrator(dist=self.poni.dist,
+                                     poni1=self.poni.poni1, 
+                                     poni2=self.poni.poni2, 
+                                     rot1=self.poni.rot1,
+                                     rot2=self.poni.rot2,
+                                     rot3=self.poni.rot3,
+                                     wavelength=self.poni.wavelength,
+                                     detector=self.poni.detector)
+            
+            result = ai.integrate1d(self.map_norm, numpoints, unit=unit,
+                                    radial_range=radial_range, mask=self.mask,
+                                    **kwargs)
+            
+            if unit == units.TTH_DEG:
+                self.int_1d_2theta = result.radial
+                self.int_1d_q = ((4*np.pi/self.poni.wavelength*1e10) 
+                                 * np.sin(np.radians(self.int_1d_2theta/2)))
+            elif unit == units.Q_A:
+                self.int_1d_q = result.radial
+                self.int_1d_q = 2*np.degrees(
+                                    np.asin(self.int_1d_q*self.poni.wavelength
+                                            * 1e10/(4*np.pi)
+                                        )
+                                    )
+            # TODO: implement other unit options for unit
+            self.int_1d_pcount = result._count
+            self.int_1d_raw = result._sum_signal
+            self.int_1d_norm = self.int_1d_raw/self.int_1d_pcount
+    
+    
+    def integrate_2d(self):
+        with self.arch_lock:
+            pass
+    
+    
+    def set_map_raw(self, new_data):
+        with self.arch_lock:
+            self.map_raw = new_data
+    
+    
+    def set_PONI(self, new_data):
+        with self.arch_lock:
+            self.PONI = new_data
+            
+    
+    def set_mask(self, new_data):
+        with self.arch_lock:
+            self.mask = new_data
+    
+    
+    def set_scan_info(self, new_data):
+        with self.arch_lock:
+            self.scan_info = new_data
+    
